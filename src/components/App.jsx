815cb8cf86e907e01067d7d31086ce7598806b78
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-07-03 00:15:55 +0300

    Finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,62 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import NotificationMessage from './NotificationMessage';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import feedbackOptions from 'components/feedbackGrades';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  changeFeedback = grade => {
-    return this.setState(prevState => ({ [grade]: prevState[grade] + 1 }));
-  };
+  const total = good + neutral + bad;
 
-  countTotalFeedback = () => {
-    const { good, bad, neutral } = this.state;
-    return good + bad + neutral;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedbacks = this.countTotalFeedback();
-    const { good } = this.state;
-    const preparedResult = `${Math.ceil((good / totalFeedbacks) * 100)}%`;
-    return totalFeedbacks === 0 ? '0%' : preparedResult;
-  };
-
-  render() {
-    const { good, bad, neutral } = this.state;
-    const total = this.countTotalFeedback();
-    return (
-      <div className="container">
-        <Section title="Leave your feedback">
-          <FeedbackOptions
-            options={feedbackOptions}
-            onLeaveFeedback={this.changeFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {!total ? (
-            <NotificationMessage title="No feedback given" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </div>
-    );
+  function changeGrade(grade) {
+    switch (grade) {
+      case 'good':
+        setGood(good => good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral => neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad => bad + 1);
+        break;
+      default:
+        return;
+    }
   }
+
+  function countPositiveFeedbackPercentage() {
+    const preparedResult = `${Math.ceil((good / total) * 100)}%`;
+    return total === 0 ? '0%' : preparedResult;
+  }
+
+  return (
+    <div className="container">
+      <Section title="Leave your feedback">
+        <FeedbackOptions
+          options={feedbackOptions}
+          onLeaveFeedback={changeGrade}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {!total ? (
+          <NotificationMessage title="No feedback given" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </div>
+  );
 }
+
+export { App };
